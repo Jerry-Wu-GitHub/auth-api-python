d3fbeedeb31952e0c552492a7b19fffa97d9e95d
Jerry <15053422+Jerry-Wu-Gitee@user.noreply.gitee.com>
2026-07-27 18:54:26 +0800

    fix: double api prefix

--- routes/api/__init__.py
+++ routes/api/__init__.py
@@ -9,17 +9,17 @@ from .v1 import (
 
 
 def _wrapper(get_router: Callable[..., APIRouter]) -> Callable[..., APIRouter]:
-    def new_get_router(*args, **kwargs) -> APIRouter:
+    def get_api_router(*args, **kwargs) -> APIRouter:
         # 添加 /api 前缀
         router = APIRouter(prefix=f"/api", tags=["API"])
         router.include_router(get_router(*args, **kwargs))
         return router
-    return new_get_router
+    return get_api_router
 
 
 get_v1_router = _wrapper(get_v1_router)
 
 
 router_getters = {
-    API_VERSION_V1: _wrapper(get_v1_router)
+    API_VERSION_V1: get_v1_router
 }
--- routes/api/v1/__init__.py
+++ routes/api/v1/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from ._common import API_VERSION, API_PREFIX
+from ._common import API_VERSION
 from .sessions import get_router as get_session_router
 
 
 def get_router(*args, **kwargs) -> APIRouter:
-    router = APIRouter(prefix=f"/{API_VERSION}", tags=["Ver. 1"])
+    router = APIRouter(prefix=f"/{API_VERSION}")
     router.include_router(get_session_router(*args, **kwargs))
     return router
 
